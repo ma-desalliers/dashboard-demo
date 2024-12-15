5c3ef1bbb2d9f3c3ff46843f20d96b8c19d8b2fe
acaumartin-cameleon <a.caumartin@cameleonmedia.com>
2024-12-14 22:31:24 -0500

    Push new repositories fake data

---
+++ src/repository/market.ts
@@ -0,0 +1,67 @@
+const markets = [
+  {
+    "uuid": "16cb3f3d-2ce9-4b4e-ac4c-1a2f99bfd4d4",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "c83b1622-1b0a-46e6-bf9a-4f823c979e69",
+    "personaUuid": "47b77bf2-bd70-4b35-8ad8-7b00fa38bb6e",
+    "createdAt": 1734131642624
+  },
+  {
+    "uuid": "e8906a60-ea2a-41b5-ab9f-a297bdcc83f7",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "5d80575e-1cdd-41b3-9dab-9c48b67f2257",
+    "personaUuid": "ec12c3e6-37c2-40e4-85f6-aedc4bd6c5df",
+    "createdAt": 1734131913885
+  },
+  {
+    "uuid": "59640101-6476-47c7-b95c-858b51065c7e",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "6b0fc750-b125-467b-8c33-e4af96f53986",
+    "personaUuid": "f0c3ed6a-dadf-4dfe-824b-aff001dd71c9",
+    "createdAt": 1734132043107
+  },
+  {
+    "uuid": "613d25fc-c5fc-4e21-9740-5e5dfab9f0d1",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "a40b011f-cf75-4408-b135-afa463ce5d37",
+    "personaUuid": "d6dec823-d63f-4f6e-b61c-e1f4048780f5",
+    "createdAt": 1734132199788
+  },
+  {
+    "uuid": "c54cb42b-1ec4-4c44-ae94-069180f1039a",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "6b0fc750-b125-467b-8c33-e4af96f53986",
+    "personaUuid": "c91297e9-8e12-4e8d-907c-7fce6197c7e9",
+    "createdAt": 1734132349345
+  },
+  {
+    "uuid": "38a5fb0f-b25e-4ced-9f81-869fef12c408",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "614fe64e-7c96-492e-9665-5c058cc6d47a",
+    "personaUuid": "bc80656b-63dd-4484-ab36-a071d798c9a0",
+    "createdAt": 1734132559217
+  },
+  {
+    "uuid": "b5bfd691-5eb0-4f06-a46e-c6f19820f6af",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "60305edd-54e0-462d-8e72-07e7ae0da5cf",
+    "personaUuid": "563957e0-9bd6-4a84-aefd-297d08309661",
+    "createdAt": 1734132730839
+  },
+  {
+    "uuid": "996a62f1-d436-4639-8d54-c0170953c539",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "34e78502-a3e0-410a-9535-6f39a9e35592",
+    "personaUuid": "73522c22-be3e-4b49-bac3-327052ee30e6",
+    "createdAt": 1734132851134
+  },
+  {
+    "uuid": "4798158f-02f8-41ce-b5e7-4ed8f9de45ce",
+    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
+    "productUuid": "6b0fc750-b125-467b-8c33-e4af96f53986",
+    "personaUuid": "daeab466-ad38-45fc-be21-f656f10e4dff",
+    "createdAt": 1734104486722
+  }
+];
+
+export default markets;
