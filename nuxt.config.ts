069f13a3d7262e03a98743e4e60933485737d555
Cameleon-MarcAndre <m.desalliers@cameleonmedia.com>
2024-12-18 10:20:29 -0500

    fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css' }
       ]
     },
-    baseURL: '/Abratech'
+    baseURL: '/abratech'
   },
   build: {
     transpile: ['perfect-scrollbar']
