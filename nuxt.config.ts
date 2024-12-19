3ca3213a71a2fdd0afd655932ce392e0b3cf97fc
Cameleon-MarcAndre <m.desalliers@cameleonmedia.com>
2024-12-19 14:49:32 -0500

    personalize project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      title: 'Atlas', // Set the global title here
+      title: 'PetalMD', // Set the global title here
       link: [
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css' }
       ]
