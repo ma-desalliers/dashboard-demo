7a3aa404805452dfbe2d961ca4463432edd567ed
acaumartin-cameleon <a.caumartin@cameleonmedia.com>
2024-12-19 14:12:12 -0500

    Push base data

--- src/repository/market.ts
+++ src/repository/market.ts
@@ -1,66 +1,87 @@
 const markets = [
   {
-    "uuid": "16cb3f3d-2ce9-4b4e-ac4c-1a2f99bfd4d4",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "c83b1622-1b0a-46e6-bf9a-4f823c979e69",
-    "personaUuid": "47b77bf2-bd70-4b35-8ad8-7b00fa38bb6e",
-    "createdAt": 1734131642624
+    "uuid": "4d929307-4945-4030-98e9-9d2fd5b44350",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "b0c145b0-ea86-468c-984d-dc973526cd89",
+    "personaUuid": "5cb002a2-7a9f-43d0-9e2e-ce4aa7af601c",
+    "createdAt": 1734363006284
   },
   {
-    "uuid": "e8906a60-ea2a-41b5-ab9f-a297bdcc83f7",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "5d80575e-1cdd-41b3-9dab-9c48b67f2257",
-    "personaUuid": "ec12c3e6-37c2-40e4-85f6-aedc4bd6c5df",
-    "createdAt": 1734131913885
+    "uuid": "80ba3955-750e-440b-a838-e0498bd5e80f",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "2e62ba8d-8b21-47f8-8982-319d3f3e95ca",
+    "personaUuid": "5cb002a2-7a9f-43d0-9e2e-ce4aa7af601c",
+    "createdAt": 1734634330269
   },
   {
-    "uuid": "59640101-6476-47c7-b95c-858b51065c7e",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "6b0fc750-b125-467b-8c33-e4af96f53986",
-    "personaUuid": "f0c3ed6a-dadf-4dfe-824b-aff001dd71c9",
-    "createdAt": 1734132043107
+    "uuid": "733e2c4a-9651-4cb3-a407-88fe8b0e0e67",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "e2f63a74-7799-470a-a498-2b835e61604a",
+    "personaUuid": "5cb002a2-7a9f-43d0-9e2e-ce4aa7af601c",
+    "createdAt": 1734634330269
   },
   {
-    "uuid": "613d25fc-c5fc-4e21-9740-5e5dfab9f0d1",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "a40b011f-cf75-4408-b135-afa463ce5d37",
-    "personaUuid": "d6dec823-d63f-4f6e-b61c-e1f4048780f5",
-    "createdAt": 1734132199788
+    "uuid": "ccc63a0d-e99e-4579-b9ab-3fc8ebafd234",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "1f5a9813-b4fb-418a-a362-5e873ccfb5f8",
+    "personaUuid": "5cb002a2-7a9f-43d0-9e2e-ce4aa7af601c",
+    "createdAt": 1734634330269
   },
   {
-    "uuid": "c54cb42b-1ec4-4c44-ae94-069180f1039a",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "6b0fc750-b125-467b-8c33-e4af96f53986",
-    "personaUuid": "c91297e9-8e12-4e8d-907c-7fce6197c7e9",
-    "createdAt": 1734132349345
+    "uuid": "dd543dd8-8bf8-43df-8b5b-29a76e8ba069",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "2e62ba8d-8b21-47f8-8982-319d3f3e95ca",
+    "personaUuid": "f0b64e14-a730-41c0-9d08-74a8d3427262",
+    "createdAt": 1734634330269
   },
   {
-    "uuid": "38a5fb0f-b25e-4ced-9f81-869fef12c408",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "614fe64e-7c96-492e-9665-5c058cc6d47a",
-    "personaUuid": "bc80656b-63dd-4484-ab36-a071d798c9a0",
-    "createdAt": 1734132559217
+    "uuid": "e8a1e926-0977-433c-9ce7-44e843681be3",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "e2f63a74-7799-470a-a498-2b835e61604a",
+    "personaUuid": "f0b64e14-a730-41c0-9d08-74a8d3427262",
+    "createdAt": 1734634330269
   },
   {
-    "uuid": "b5bfd691-5eb0-4f06-a46e-c6f19820f6af",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "60305edd-54e0-462d-8e72-07e7ae0da5cf",
-    "personaUuid": "563957e0-9bd6-4a84-aefd-297d08309661",
-    "createdAt": 1734132730839
+    "uuid": "87177d5d-486f-47e5-af17-1356b92b3d27",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "b0c145b0-ea86-468c-984d-dc973526cd89",
+    "personaUuid": "f0b64e14-a730-41c0-9d08-74a8d3427262",
+    "createdAt": 1734634330269
   },
   {
-    "uuid": "996a62f1-d436-4639-8d54-c0170953c539",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "34e78502-a3e0-410a-9535-6f39a9e35592",
-    "personaUuid": "73522c22-be3e-4b49-bac3-327052ee30e6",
-    "createdAt": 1734132851134
+    "uuid": "fcab8c01-61d8-43e3-9e74-61db767eddb1",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "1f5a9813-b4fb-418a-a362-5e873ccfb5f8",
+    "personaUuid": "f0b64e14-a730-41c0-9d08-74a8d3427262",
+    "createdAt": 1734634330269
   },
   {
-    "uuid": "4798158f-02f8-41ce-b5e7-4ed8f9de45ce",
-    "clientUuid": "75b211f3-0b75-4bd6-8423-bd1526fea9c0",
-    "productUuid": "6b0fc750-b125-467b-8c33-e4af96f53986",
-    "personaUuid": "daeab466-ad38-45fc-be21-f656f10e4dff",
-    "createdAt": 1734104486722
+    "uuid": "8f058038-0b8a-49e3-aff5-dc0d9ef63ff8",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "2e62ba8d-8b21-47f8-8982-319d3f3e95ca",
+    "personaUuid": "abc6c80e-4a25-46cb-9724-d89b36bf35c2",
+    "createdAt": 1734634330269
+  },
+  {
+    "uuid": "c64bd32a-2989-4fbe-87b4-b14aca872cc4",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "e2f63a74-7799-470a-a498-2b835e61604a",
+    "personaUuid": "abc6c80e-4a25-46cb-9724-d89b36bf35c2",
+    "createdAt": 1734634330269
+  },
+  {
+    "uuid": "2300fc4b-93ef-4890-8ccf-fec7b0eeb9d1",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "b0c145b0-ea86-468c-984d-dc973526cd89",
+    "personaUuid": "abc6c80e-4a25-46cb-9724-d89b36bf35c2",
+    "createdAt": 1734634330269
+  },
+  {
+    "uuid": "e1b31266-bc5a-4c07-bab1-d6698a2372f0",
+    "clientUuid": "d09b914b-451d-4949-a9b2-eb2f8559809d",
+    "productUuid": "31125392-512d-4d0a-b30a-bd965e61590b",
+    "personaUuid": "6544235d-1aa1-41ee-a040-613381586a23",
+    "createdAt": 1734634813845
   }
 ];
 
