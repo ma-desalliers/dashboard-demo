262f7a3475dbf7c9144a0d4bf00bc95ac9e8c957
Cameleon-MarcAndre <m.desalliers@cameleonmedia.com>
2024-12-23 13:35:08 -0500

    fic name

--- src/repository/client.ts
+++ src/repository/client.ts
@@ -1,5 +1,5 @@
 const company = {
-  name:'Pompes Garands',
+  name:'Pompes Garand',
   folder:'pompesgarands',
   logo:`https://lirp.cdn-website.com/e3c7e386/dms3rep/multi/opt/LOGO_new_pompegarand5FINAL-01-d1fd8152-250w.jpg`,
   logoType:'img',
