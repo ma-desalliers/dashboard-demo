77d0e57d4b9e2e7b2145bf781e63c9c5d5a6456e
Cameleon-MarcAndre <m.desalliers@cameleonmedia.com>
2024-12-23 13:39:59 -0500

    added folder name

--- src/repository/client.ts
+++ src/repository/client.ts
@@ -1,5 +1,6 @@
 const company = {
   name:'Jupiter Machinerie',
+  folder:'jupitermachinerie',
   logo:`https://www.google.com/s2/favicons?domain=jupitermachinerie.ca&sz=256`,
   logoType:'img',
   primaryColor:'#1c4a96',
