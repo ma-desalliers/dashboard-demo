47b53bb0963b56ef470f04c6fbf390e97c93001e
Cameleon-MarcAndre <m.desalliers@cameleonmedia.com>
2024-12-23 13:30:45 -0500

    set folder name

--- src/repository/client.ts
+++ src/repository/client.ts
@@ -1,5 +1,6 @@
 const company = {
   name:'Beton Provincial',
+  folder:'betonprovincial',
   logo:'https://www.google.com/s2/favicons?domain=betonprovincial.com&sz=256',
   logoType:'img',
   primaryColor:'#f3912d',
