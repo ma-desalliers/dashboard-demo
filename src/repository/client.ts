e94d7deb4508195434179286acd39f366bf76a3f
Cameleon-MarcAndre <m.desalliers@cameleonmedia.com>
2024-12-23 13:34:28 -0500

    set folder name

--- src/repository/client.ts
+++ src/repository/client.ts
@@ -1,5 +1,6 @@
 const company = {
   name:'Pompes Garands',
+  folder:'pompesgarands',
   logo:`https://lirp.cdn-website.com/e3c7e386/dms3rep/multi/opt/LOGO_new_pompegarand5FINAL-01-d1fd8152-250w.jpg`,
   logoType:'img',
   primaryColor:'#A9281F',
